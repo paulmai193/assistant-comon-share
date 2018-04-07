0aefbb4560e34bd0dbdb6cc7652526e11779aabf
Dai Mai <dai.mai@mediastep.com>
2018-04-07 12:22:06 +0700

    add compiler plugin

--- src/main/java/logia/assistant/share/common/entity/AbstractUuidEntity.java
+++ src/main/java/logia/assistant/share/common/entity/AbstractUuidEntity.java
@@ -4,17 +4,31 @@ import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.Size;
 
+/**
+ * The Class AbstractUuidEntity.
+ *
+ * @author Dai Mai
+ */
 @MappedSuperclass
-public class AbstractUuidEntity extends AbstractAuditingEntity {
+public abstract class AbstractUuidEntity extends AbstractAuditingEntity {
 
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = 1L;
+    
+    /** The uuid. */
     @Size(min = 1, max = 10)
     @Column(length = 10, unique = true, nullable = false)
     private String uuid;
     
+    /**
+     * Instantiates a new abstract uuid entity.
+     */
     public AbstractUuidEntity() {
     }
     
     /**
+     * Gets the uuid.
+     *
      * @return the uuid
      */
     public String getUuid() {
@@ -22,6 +36,8 @@ public class AbstractUuidEntity extends AbstractAuditingEntity {
     }
     
     /**
+     * Sets the uuid.
+     *
      * @param uuid the uuid to set
      */
     public void setUuid(String uuid) {
