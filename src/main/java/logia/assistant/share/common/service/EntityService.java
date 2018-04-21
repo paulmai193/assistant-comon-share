0b8754e3673e8010caadc071fe9557fa3038cfba
Dai Mai <dai.mai@mediastep.com>
2018-04-21 10:45:07 +0700

    create interface for entity service

---
+++ src/main/java/logia/assistant/share/common/service/EntityService.java
@@ -0,0 +1,92 @@
+package logia.assistant.share.common.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * The Interface EntityService.
+ *
+ * @author Dai Mai
+ * @param <D> the generic type
+ * @param <E> the element type
+ * @param <K> the ID type
+ */
+public interface EntityService<D, E, K extends Serializable> {
+    
+    /**
+     * Save the entity.
+     *
+     * @param dto the entity mapped to DTO
+     * @return the saved entity mapped to DTO
+     */
+    D saveDto(D dto);
+    
+    /**
+     * Save the entity.
+     *
+     * @param entity the entity
+     * @return the saved entity
+     */
+    E saveEntity(E entity);
+    
+    /**
+     * Find all entities.
+     *
+     * @return the list entities mapped to DTOs
+     */
+    List<D> findAll();
+    
+    /**
+     * Find all entities.
+     *
+     * @return the list entities
+     */
+    List<D> findAllEntities();
+    
+    /**
+     * Find the "id" entity mapped to DTO.
+     *
+     * @param id the id
+     * @return the DTO
+     */
+    D findOneDto(K id);
+    
+    /**
+     * Find the "id" entity.
+     *
+     * @param id the id
+     * @return the entity
+     */
+    E findOneEntity(K id);
+    
+    /**
+     * Delete the "id" entity.
+     *
+     * @param id the id
+     */
+    void delete(K id);
+    
+    /**
+     * Search for the entities corresponding to the query.
+     *
+     * @param query the query of the search
+     * @return the list of entities mapped to DTOs
+     */
+    List<D> search(String query);
+    
+    /**
+     * Map entity to DTO.
+     *
+     * @param entity the entity
+     * @return the DTO
+     */
+    D mapToDto(E entity);
+    
+    /**
+     * Map DTO to entity.
+     *
+     * @param dto the DTO
+     * @return the entity
+     */
+    E mapToEntity(D dto);
+}
